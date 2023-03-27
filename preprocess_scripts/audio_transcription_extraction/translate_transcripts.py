558d0c5a4226c2ff8f8c1adf5b5acd9a6cb19618
digbose92 <digbose92@gmail.com>
2023-03-27 13:12:48 -0700

    Added language classifier results

--- preprocess_scripts/audio_transcription_extraction/translate_transcripts.py
+++ preprocess_scripts/audio_transcription_extraction/translate_transcripts.py
@@ -1,12 +1,16 @@
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
 #mbart model and tokenizer declaration 
-model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-one-mmt")
+tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-one-mmt")
 
 #device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #load model to device
 model.to(device)
+
+#sample text 
+
+
 
