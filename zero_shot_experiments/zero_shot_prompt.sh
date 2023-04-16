6bf4eb28e399281802dc5ffc9e557b1308837c00
tiantiaf <tiantiaf@usc.edu>
2023-04-16 03:19:22 -0700

    minor prompt update

--- zero_shot_experiments/zero_shot_prompt.sh
+++ zero_shot_experiments/zero_shot_prompt.sh
@@ -1,3 +1,4 @@
 
 
-python transcript_zero_shot_classify_flan.py --model_name flan-t5-xl --task_name topic --cache_folder /media/data/public-data/llm --dest_folder /media/data/public-data/ads_complete_repo/ads_transcripts/combined_transcripts/en_combined_transcripts.json
+python transcript_zero_shot_classify_flan.py --model_name flan-t5-xl --task_name social_message --cache_folder /media/data/public-data/llm --dest_folder /media/data/public-data/ads_complete_repo/ads_transcripts/combined_transcripts/en_combined_transcripts.json
+# python evaluate_local_zero_shot_models.py --model_name flan-t5-xl --task_name social_message
